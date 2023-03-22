139df42c6cb88f8a2d6a2ec84c33e18127d3725c
Joao Fernando <joao.fernando@a.ucb.br>
2023-03-21 21:04:59 -0300

    subindo arquivos

---
+++ lista08.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int A, B, C, D;
+
+    scanf("%d", A);
+    scanf("%d", B);
+    scanf("%d", C);
+    scanf("%d", D);
+
+    printf("(%d + %d) * (%d + %d) ");
+
+    return 0;
+}
